950e3590bccbb4022098fa7bc973034afb4437b0
luisOrtga <ortegaluiskb@gmail.com>
2026-09-06 12:56:46 -0600

    Update v.1.0

--- tasks/models.py
+++ tasks/models.py
@@ -7,3 +7,6 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     done = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'{self.title}'
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Task
+from .serializer import TaskSerializer
+
 
 # Create your views here.
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()

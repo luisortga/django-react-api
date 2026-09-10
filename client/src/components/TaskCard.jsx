7ee4e76e0af406120738e6db32146772d4f02904
luisOrtga <ortegaluiskb@gmail.com>
2026-09-09 23:51:08 -0600

    Init delete task

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -2,7 +2,7 @@
 
 export function TaskCard({ task }) {
     return (
-        <div>
+        <div style={{background: "#028a6d"}} >
                 <h1>{task.title}</h1>
                 <p>{task.description}</p>
         </div>
